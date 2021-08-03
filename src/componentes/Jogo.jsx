fbf55be86b45d35951c242c70ee64dcdf4e872a5
Christina Fernandes <christina.o.fernandes21@gmail.com>
2021-08-03 00:40:45 -0300

    attAlert2

--- src/componentes/Jogo.jsx
+++ src/componentes/Jogo.jsx
@@ -31,11 +31,6 @@ export default class Jogo extends React.Component {
         if (escolhaApp == "pedra") {
             if (escolhaUser == "pedra") {
                 resultado = "Empate";
-                // alert(
-                //     <Image
-                //         source={require('./img/empate.png')}
-                //     />
-                // )
             }
             if (escolhaUser == "papel") {
                 resultado = "User Ganhou";
